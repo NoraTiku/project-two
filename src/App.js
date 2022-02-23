7a75eb4eb82cf42758fb3d4f133573b5ae0caa20
NoraTiku <jtiku@madisoncollege.edu>
2022-02-22 22:55:51 -0600

    created a new tract component names Numbers
    and put it in it's own file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Numbers from './Numbers';
+
+
+
+
 
 const App = () => {
   return (
+    <>
     <h1>Mini Project Two App!!!</h1>
+    <h1>Hello John</h1>
 
+    <Numbers/>
+    <Numbers/>
+    <Numbers/>
     
+    
+
+    </>
   );
 }
 
---
+++ src/Numbers.js
@@ -0,0 +1,8 @@
+const Numbers = () => {
+
+    return (
+      <h2>Hi There</h2>
+    );
+  };
+
+  export default Numbers;
